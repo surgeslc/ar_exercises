65b59c7831035431901ab45ece0a2197a64bbd1f
Lawrence Surges <surgeslc@gmail.com>
2017-08-02 00:38:57 +0000

    Completes Exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,14 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Store < ActiveRecord::Base
+end
+
+Store.create({name: 'Burnaby', annual_revenue: 300000, mens_apparel: true,
+  womens_apparel: true})
+
+Store.create({name: 'Richmond', annual_revenue: 1260000, womens_apparel: true})
+
+Store.create({name: 'Gastown', annual_revenue: 190000, mens_apparel: true})
+
+puts Store.count
