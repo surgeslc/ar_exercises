51147362e09d337ad02b32fcd90be4d28ff4cfd8
Lawrence Surges <surgeslc@gmail.com>
2017-08-02 01:25:12 +0000

    Completes Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.update(name: 'Metrotown')
+
+puts 'Name of Store 1 changed to ' + @store1.name
