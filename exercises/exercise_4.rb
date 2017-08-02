a580e5d64c3aa7d1505650db80a343c162850e73
Lawrence Surges <surgeslc@gmail.com>
2017-08-02 17:49:28 +0000

    Exercise 4 pre-refactoring

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+Store.create({name: 'Surrey', annual_revenue: 224000, womens_apparel: true})
+
+Store.create({name: 'Whistler', annual_revenue: 1900000, mens_apparel: true})
+
+Store.create({name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true})
+
+@mens_stores = Store.where(mens_apparel: 1)
+puts "Mens Stores:"
+puts "----------"
+@mens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
+
+@selected_stores = Store.where("annual_revenue < ?", 1000000).where(womens_apparel: 1)
+@selected_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
